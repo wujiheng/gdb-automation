6803e93f49fcdaf4ae990da51f71ab658b2ec196
Wujiheng <n26014809@mail.ncku.edu.tw>
2013-10-21 20:52:21 +0800

    Add comments in check.py

--- check.py
+++ check.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python
 
-import os
+import os       #for using operation system function
 
+#open qemu serial output result
 fin = open( './serial.out', 'r' )
+
+#read each line in the file and compare to "Hello"
+#if find, it means USART output"Hello" successful
 for line in fin.readlines():
     if line.find( "Hello" ):
         print "Find string : \"Hello\" in serial"
     else :
         print "Can't fin string : \"Hello\" "
+
+#file close
 fin.close()
 
+#kill qemu
 os.system( 'killall qemu-system-arm' )
